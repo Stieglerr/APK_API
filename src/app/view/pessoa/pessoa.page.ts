330db2e86727ab3309c3e366420bd50aa5d4b5d2
Stieglerr <stieglerstr@hotmail.com>
2025-08-20 18:55:14 -0300

    1

---
+++ src/app/view/pessoa/pessoa.page.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { arrowBack } from 'ionicons/icons';
+import { Invertexto } from 'src/app/service/invertexto';
+import { Router } from '@angular/router';
+
+addIcons({
+  arrowBack: arrowBack
+})
+
+@Component({
+  selector: 'app-pessoa',
+  templateUrl: './pessoa.page.html',
+  styleUrls: ['./pessoa.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})  
+export class PessoaPage implements OnInit {
+  pessoa: any = null;
+  errorMessage: string = '';
+
+  constructor(private invertextoService: Invertexto,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.getPessoa();
+  }
+getPessoa(){
+  this.invertextoService.getPessoa().subscribe(
+    (response) => {
+      this.pessoa = response;
+      this.errorMessage = ''
+    },
+    (error) => {
+      console.error('Erro na requisição', error);
+      this.errorMessage = 'Erro ao gerar pessoa, tente novamente';
+    }
+  );
+}
+goToHome(){
+  this.router.navigate(['/home']);
+}
+}
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { HttpClientModule, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(withInterceptorsFromDi()),
+    HttpClientModule
   ],
 });
